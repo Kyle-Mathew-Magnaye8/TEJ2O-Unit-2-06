8426d3706ef4cec08079d5bc8f0da88925b91ab5
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2026-02-27 18:09:34 +0000

    updated main.py code

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,9 +1,19 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Kyle Matthew
+Created on: Feb 2026
+This module shows the LED on and off using A and B buttons.
 """
 
 from microbit import *
 
-display.scroll("Hello, World!")
+
+display.clear()
+display.show(Image.HAPPY)
+
+while True:
+    if button_a.is_pressed():
+        pin16.write_digital(1)
+        display.show(Image.YES)
+    if button_b.is_pressed():
+        pin16.write_digital(0)
+        display.show(Image.NO)
